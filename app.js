853983becfb453e695298baf35629dbc6c5d4783
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-22 13:27:33 -0400

    Font Change

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var emailButton = document.getElementById('emailButton');
 emailButton.onclick = function() {
+    window.open('mailto:hemphilltaylor@gmail.com')
+    // "parent.location='mailto:hemphilltaylor@gmail.com'"
     alert('hemphilltaylor@gmail.com')
 }
+
+
