0318d72a8b4338fc9e7a072b55b79719fc2db42d
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-22 14:25:51 -0400

    Email button and About Me Update

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var emailButton = document.getElementById('emailButton');
 emailButton.onclick = function() {
     window.open('mailto:hemphilltaylor@gmail.com')
-    // "parent.location='mailto:hemphilltaylor@gmail.com'"
-    alert('hemphilltaylor@gmail.com')
 }
 
 
