983e1e6c8fa6dccba7dc92665000d758e6849244
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-23 10:43:00 -0400

    organization and buttons

---
+++ About/about.js
@@ -0,0 +1,15 @@
+
+var gitButton = document.getElementById('gitButton');
+gitButton.onclick = function() {
+    window.open('https://github.com/THemphill23', '_blank')
+}
+
+var linkedButton = document.getElementById('linkedButton');
+linkedButton.onclick = function() {
+    window.open("https://www.linkedin.com/in/taylorhemphill1123", "_blank")
+}
+
+var tweetButton = document.getElementById('tweetButton');
+tweetButton.onclick = function() {
+    window.open("https://twitter.com/themphill23", "_blank")
+}
---
+++ Index/app.js
@@ -0,0 +1,5 @@
+var emailButton = document.getElementById('emailButton');
+emailButton.onclick = function() {
+    window.open('mailto:hemphilltaylor@gmail.com')
+}
+
