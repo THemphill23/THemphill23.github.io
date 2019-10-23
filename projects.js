ccca0e19ecff18d9b178a902059c658bcd582036
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-23 14:03:57 -0400

    Replace Buttons for Links

---
+++ projects.js
@@ -0,0 +1,24 @@
+var demoButton = document.getElementById('demoButton');
+demoButton.onclick = function() {
+    window.open("https://youtu.be/xyEOq86A1xc", "_blank")
+}
+
+var petsButton = document.getElementById('petsButton');
+petsButton.onclick = function() {
+    window.open("https://github.com/THemphill23/VirtualPets", "_blank")
+}
+
+var reviewButton = document.getElementById('reviewButton');
+reviewButton.onclick = function() {
+    window.open("https://github.com/THemphill23/MvcProject-1", "_blank")
+}
+
+var hospitalButton = document.getElementById('hospitalButton');
+hospitalButton.onclick = function() {
+    window.open("https://github.com/THemphill23/MasteryHospital", "_blank")
+}
+
+var blogButton = document.getElementById('blogButton');
+blogButton.onclick = function() {
+    window.open("https://github.com/THemphill23/MvcMasteryBlog", "_blank")
+}
