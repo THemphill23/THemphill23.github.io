f0b6ff1a93a925113800e268715972b0e51e6699
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-23 09:23:40 -0400

    More Buttons

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ emailButton.onclick = function() {
     window.open('mailto:hemphilltaylor@gmail.com')
 }
 
+var gitButton = document.getElementById('gitButton');
+gitButton.onclick = function() {
+    window.open("https://github.com")
+}
 
+var linkedButton = document.getElementById('linkedButton');
+linkedButton.onclick = function() {
+    window.open("https://www.linkedin.com")
+}
+
+var openButton = document.getElementById('openButton');
+openButton.onclick = function() {
+  window.open("https://www.w3schools.com")
+}
