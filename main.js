81132d95e50b5c1bb090a250c6209c211c77193c
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-12-10 11:43:45 -0500

    Style and Button JS updates

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const linkedButton = document.querySelector("#linkedButton");
 const gitButton = document.querySelector("#gitButton");
 const projectsButton = document.querySelector("#projectButton");
 
+const clashButton = document.querySelector("#clashButton");
+const smallButton = document.querySelector("#smallButton");
+const reviewButton = document.querySelector("#reviewButton");
+const albumButton = document.querySelector("#albumButton");
+const blogButton = document.querySelector("#blogButton");
+const demoButton = document.querySelector("#demoButton");
+
 emailButton.addEventListener("click", function(){
     window.open('mailto:hemphilltaylor@gmail.com', '_blank')
 });
@@ -18,3 +25,30 @@ gitButton.addEventListener("click", function(){
 projectsButton.addEventListener("click", function(){
     window.scrollTo(0, 1000)
 })
+
+/*Projects*/
+clashButton.addEventListener("click", function(){
+    window.open("https://github.com/THemphill23/ApiMastery")
+});
+
+smallButton.addEventListener("click", function(){
+    window.open("https://github.com/THemphill23/Focus")
+});
+
+reviewButton.addEventListener("click", function(){
+    window.open("https://github.com/THemphill23/MvcProject-1")
+});
+
+albumButton.addEventListener("click", function(){
+    window.open("https://github.com/THemphill23/AlbumProject")
+});
+
+blogButton.addEventListener("click", function(){
+    window.open("https://github.com/THemphill23/MvcMasteryBlog")
+});
+
+demoButton.addEventListener("click", function(){
+    window.open("https://youtu.be/xyEOq86A1xc")
+});
+
+/*Projects*/
